cfa3a83f6f931ff4678ae7f59d2d83dd1020aa48
test12270 <tykim1227@gmail.com>
2023-06-27 19:54:01 +0900

    dev_#feat98 : ProductDetailPage DirectPayment develop

--- src/pages/Product/ProductDetail.tsx
+++ src/pages/Product/ProductDetail.tsx
@@ -27,6 +27,7 @@ import ErrorModal from "../../components/Modal/ErrorHandleModal";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import Loading from "../../components/Useable/Loading";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import { RequestPayParams, RequestPayResponse } from "../../atom/PortOne";
 
 const DetailTop = styled.div`
   width: 1180px;
@@ -206,6 +207,84 @@ const ProductDetail = () => {
   );
   // console.log(cartItems);
 
+  const onSubmitOrder = async () => {
+    try {
+      const response = await api.post("cal/v1/order", {
+        orderProducts: [{ productId: id, quantity: 1 }],
+      });
+      const responseData = response.data.body.order;
+      if (response && responseData) {
+        const res = await api.post("cal/v1/verifyIamport", {
+          amount: responseData.totalAmount,
+          orderNumber: responseData.orderNumber,
+        });
+        if (res) {
+          if (!window.IMP) {
+            return;
+          } else {
+            const { IMP } = window;
+            IMP.init("imp48116556");
+
+            const ProductsLength = responseData.orderProducts.length;
+            let OtherProducts = "";
+
+            if (ProductsLength > 1) {
+              OtherProducts = `${responseData.orderProducts[0].name} 외${
+                ProductsLength - 1
+              } 개`;
+            } else if (ProductsLength == 1) {
+              OtherProducts = responseData.orderProducts[0].name;
+            }
+
+            const data: RequestPayParams = {
+              pg: "html5_inicis.INIBillTst", // PG사 : https://portone.gitbook.io/docs/sdk/javascript-sdk/payrq#undefined-1 참고
+              pay_method: "card", // 결제수단
+              merchant_uid: `${responseData.orderNumber}`, // 주문번호
+              amount: Number(`${responseData.totalAmount}`),
+              name: OtherProducts,
+              buyer_name: `${responseData.name}`, // 구매자 이름
+              buyer_tel: `${responseData.phoneNumber}`, // 구매자 전화번호
+              buyer_email: `${responseData.email}`, // 구매자 이메일
+              buyer_addr: `${responseData.address} ${responseData.addressDetail}`,
+            };
+            IMP.request_pay(data, callback);
+          }
+        }
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        handleOpenErrorModal(error.response?.data.message);
+      }
+    }
+  };
+
+  const callback = async (response: RequestPayResponse) => {
+    const { success, error_msg } = response;
+
+    if (success) {
+      alert("결제 성공");
+
+      const res = await api.put(
+        `http://43.201.170.8:8000/cal/v1/verifyIamport/${response.merchant_uid}`,
+        `${response.imp_uid}`,
+        {
+          headers: {
+            "Content-Type": "text/plain",
+          },
+        }
+      );
+      if (res) {
+        await api.post(`cal/v1/order/${res.data.orderId}/directComplete`);
+      }
+      console.log(response);
+      console.log(response.imp_uid);
+      console.log(response.merchant_uid);
+      setCartItems([]);
+    } else {
+      handleOpenErrorModal(error_msg);
+    }
+  };
+
   return isLoading ? (
     <Loading />
   ) : (
@@ -304,6 +383,7 @@ const ProductDetail = () => {
                 borderColor: "#000",
                 backgroundColor: "#000",
               }}
+              onClick={onSubmitOrder}
             >
               바로구매
             </Button>
