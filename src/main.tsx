6800a380516549bc00ccc9adf3e3c58e5faf2a56
test12270 <tykim1227@gmail.com>
2023-07-01 23:30:15 +0900

    배포 오류수정 테스트

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "quill/dist/quill.snow.css";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Quill from "quill";
 
 const queryClient = new QueryClient();
 
