4e6eff6c5276e46a27b818bd4b771b3969e1ac6f
test12270 <tykim1227@gmail.com>
2023-07-05 13:19:09 +0900

    dev_#feat159 : 카카오 로그인 테스트 - post => direct 변경

--- src/components/Useable/KakaoLogin.tsx
+++ src/components/Useable/KakaoLogin.tsx
@@ -1,33 +1,33 @@
 import KakaoLoginImg from "../../dummy/img/kakaologin.png";
-import { api } from "../../atom/apiCall";
+// import { api } from "../../atom/apiCall";
 
 const KakaoLogin = () => {
-  const handleKaKaoLogin = async () => {
-    console.log("Before request"); // 요청 전에 출력
-    const response = await api.post("/oauth2/authorization/kakao");
-    console.log("After request"); // 요청 후에 출력
-    // 로그인 성공 후 리다이렉트된 URL 가져오기
-    const redirectUrl = response.request.responseURL;
+  //   const handleKaKaoLogin = async () => {
+  //     console.log("Before request"); // 요청 전에 출력
+  //     const response = await api.post("/oauth2/authorization/kakao");
+  //     console.log("After request"); // 요청 후에 출력
+  //     // 로그인 성공 후 리다이렉트된 URL 가져오기
+  //     const redirectUrl = response.request.responseURL;
 
-    // URL의 쿼리 파라미터 추출
-    const urlParams = new URLSearchParams(new URL(redirectUrl).search);
+  //     // URL의 쿼리 파라미터 추출
+  //     const urlParams = new URLSearchParams(new URL(redirectUrl).search);
 
-    // 토큰 값 가져오기
-    const token = urlParams.get("token");
-    const refreshToken = urlParams.get("refreshToken");
+  //     // 토큰 값 가져오기
+  //     const token = urlParams.get("token");
+  //     const refreshToken = urlParams.get("refreshToken");
 
-    // 토큰 값을 사용하여 원하는 작업 수행
-    console.log(token);
-    console.log(refreshToken);
-  };
-
+  //     // 토큰 값을 사용하여 원하는 작업 수행
+  //     console.log(token);
+  //     console.log(refreshToken);
+  //   };
+  const requestURL = "https://casalot.shop/oauth2/authorization/kakao";
   return (
     <img
       alt="카카오 로그인"
       src={KakaoLoginImg}
       width="100%"
       style={{ margin: "0 auto", cursor: "pointer" }}
-      onClick={handleKaKaoLogin}
+      onClick={() => (window.location.href = requestURL)}
     />
   );
 };
