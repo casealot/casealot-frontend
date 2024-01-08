f464e80fd510a21fff4c81a65508aa3711dbad06
tae young kim <63242552+test12270@users.noreply.github.com>
2024-01-08 12:50:41 +0900

    dev_#refactor : 상품 렌더링시간 탐색

--- src/atom/apiCall.ts
+++ src/atom/apiCall.ts
@@ -5,7 +5,7 @@ export const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 10000,
+  timeout: 20000,
 });
 
 api.interceptors.request.use((config) => {
